ba290a5f7013cb9e1ea6da3b18b87480f04915bb
rogwild <rogwild.design@gmail.com>
2023-02-20 00:02:32 +0700

    packages updates

--- frontend/src/components/inputs/listbox/index.tsx
+++ frontend/src/components/inputs/listbox/index.tsx
@@ -18,7 +18,7 @@ export default function ListboxInput(props: IInputProps) {
     rules,
     options,
     shouldUnregister,
-    defaultValue,
+    defaultValue = null,
     className,
     initialValue,
     placeholder = ``,
