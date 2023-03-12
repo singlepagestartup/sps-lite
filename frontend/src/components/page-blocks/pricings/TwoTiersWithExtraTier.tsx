c3ba6278f60b3c25d08b503f41b8319ee0fc0d8a
rogwild <rogwild.design@gmail.com>
2023-03-12 18:28:26 +0700

    cards for tiers and new seed

--- frontend/src/components/page-blocks/pricings/TwoTiersWithExtraTier.tsx
+++ frontend/src/components/page-blocks/pricings/TwoTiersWithExtraTier.tsx
@@ -1,12 +1,15 @@
-import utils from "@rogwild/next-utils";
-const { getImageUrl } = utils.api;
-import Image from "next/image";
-import { IIncentives, IPricings } from "types";
-import { BACKEND_URL } from "~utils/envs";
-import { useMemo, useTransition } from "react";
+import { IPricings } from "types";
+import { useMemo } from "react";
 import { CheckIcon } from "@heroicons/react/24/outline";
 import useTranslations from "~hooks/use-translations";
 import Link from "next/link";
+import Cards, { ICardProps } from "~components/cards";
+
+const cardsConfig = {
+  emptyLength: 3,
+  Comp: TierCard,
+  className: `mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2 lg:gap-8 items-start`,
+};
 
 export default function TwoTiersWithExtraTier(props: IPricings) {
   const translate = useTranslations();
@@ -48,60 +51,12 @@ export default function TwoTiersWithExtraTier(props: IPricings) {
       <div className="flow-root bg-white pb-32 lg:pb-40">
         <div className="relative -mt-80">
           <div className="relative z-10 mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2 lg:gap-8 items-start">
-              {props.tiers?.map((tier, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col rounded-3xl bg-white shadow-xl ring-1 ring-black/10"
-                >
-                  <div className="p-8 sm:p-10">
-                    <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
-                      {tier.title}
-                    </h3>
-                    <div className="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900">
-                      {tier.price ? `$${tier.price}` : translate(`Free`)}
-                      {tier?.period ? (
-                        <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">
-                          /mo
-                        </span>
-                      ) : null}
-                    </div>
-                    <p className="mt-6 text-base leading-7 text-gray-600">
-                      {tier.description}
-                    </p>
-                  </div>
-                  <div className="flex flex-1 flex-col p-2">
-                    <div className="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 p-6 sm:p-8">
-                      <ul role="list" className="space-y-6">
-                        {tier.features?.map((feature, fIndex) => (
-                          <li key={fIndex} className="flex items-start">
-                            <div className="flex-shrink-0">
-                              <CheckIcon
-                                className="h-6 w-6 text-indigo-600"
-                                aria-hidden="true"
-                              />
-                            </div>
-                            <p className="ml-3 text-sm leading-6 text-gray-600">
-                              {feature.title}
-                            </p>
-                          </li>
-                        ))}
-                      </ul>
-                      <div className="mt-8">
-                        {tier?.url ? (
-                          <Link
-                            href={tier.url}
-                            className="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700"
-                          >
-                            Get started today
-                          </Link>
-                        ) : null}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+            <Cards
+              variant="simple"
+              items={props.tiers}
+              showSkeletons={false}
+              cardsConfig={cardsConfig}
+            />
           </div>
         </div>
         {/* <div className="relative mx-auto mt-8 max-w-7xl px-6 lg:px-8">
@@ -128,6 +83,61 @@ export default function TwoTiersWithExtraTier(props: IPricings) {
             </div>
           </div>
         </div> */}
+      </div>
+    </div>
+  );
+}
+
+function TierCard(props: ICardProps) {
+  const translate = useTranslations();
+  const { item } = props;
+
+  return (
+    <div className="flex flex-col rounded-3xl bg-white shadow-xl ring-1 ring-black/10">
+      <div className="p-8 sm:p-10">
+        <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+          {item.title}
+        </h3>
+        <div className="mt-4 flex items-baseline text-5xl font-bold tracking-tight text-gray-900">
+          {item.price ? `$${item.price}` : translate(`Free`)}
+          {item?.period ? (
+            <span className="text-lg font-semibold leading-8 tracking-normal text-gray-500">
+              /mo
+            </span>
+          ) : null}
+        </div>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          {item.description}
+        </p>
+      </div>
+      <div className="flex flex-1 flex-col p-2">
+        <div className="flex flex-1 flex-col justify-between rounded-2xl bg-gray-50 p-6 sm:p-8">
+          <ul role="list" className="space-y-6">
+            {item.features?.map((feature: any, fIndex: number) => (
+              <li key={fIndex} className="flex items-start">
+                <div className="flex-shrink-0">
+                  <CheckIcon
+                    className="h-6 w-6 text-indigo-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <p className="ml-3 text-sm leading-6 text-gray-600">
+                  {feature.title}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-8">
+            {item?.url ? (
+              <Link
+                href={item.url}
+                className="inline-block w-full rounded-lg bg-indigo-600 px-4 py-2.5 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-indigo-700"
+              >
+                Get started today
+              </Link>
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
