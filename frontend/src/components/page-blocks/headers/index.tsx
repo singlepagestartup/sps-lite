70372ad98b09a6bcc9f2006216cb9d2722e9c641
StepanTeshka <leluhinstepan@gmail.com>
2023-03-13 18:07:54 +0300

    Update index.tsx

--- frontend/src/components/page-blocks/headers/index.tsx
+++ frontend/src/components/page-blocks/headers/index.tsx
@@ -1,22 +1,3 @@
-// import { FC } from "react";
-// import { IHeaderSection, IPageProps } from "types";
-// import SimpleLinksOnLeft from "./SimpleLinksOnLeft";
-
-// const variants = {
-//   "simple-links-on-left": SimpleLinksOnLeft,
-// };
-
-// export default function HeaderSections(props: IPageProps) {
-//   const Comp = variants[
-//     props.header.variant as keyof typeof variants
-//   ] as FC<IPageProps>;
-
-//   if (!Comp) {
-//     return <></>;
-//   }
-
-//   return <Comp {...props} />;
-// }
 import { FC } from "react";
 import { IPageProps } from "types";
 import Topbar from "~components/topbar";
