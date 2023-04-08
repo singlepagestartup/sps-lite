756ddeec4a2723a2f89c594503dbadb159ac2fa9
rogwild <rogwild.design@gmail.com>
2023-04-09 01:58:55 +0300

    husky

--- backend/src/api/metatag/controllers/metatag.ts
+++ backend/src/api/metatag/controllers/metatag.ts
@@ -2,6 +2,6 @@
  * metatag controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::metatag.metatag');
--- backend/src/api/topbar/controllers/topbar.ts
+++ backend/src/api/topbar/controllers/topbar.ts
@@ -2,6 +2,6 @@
  * topbar controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::topbar.topbar');
