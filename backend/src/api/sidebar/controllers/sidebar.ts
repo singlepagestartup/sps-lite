2f49d052e423de11ab34265b3a218743dbcbeb3c
rogwild <rogwild.design@gmail.com>
2023-04-08 03:42:44 +0300

    husky

--- backend/src/api/sidebar/controllers/sidebar.ts
+++ backend/src/api/sidebar/controllers/sidebar.ts
@@ -2,6 +2,6 @@
  * sidebar controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::sidebar.sidebar');
