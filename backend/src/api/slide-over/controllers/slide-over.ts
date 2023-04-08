4254e84ce84276eb9fa24d13a0686d0d297bfb7e
rogwild <rogwild.design@gmail.com>
2023-04-08 03:12:26 +0300

    husky

--- backend/src/api/slide-over/controllers/slide-over.ts
+++ backend/src/api/slide-over/controllers/slide-over.ts
@@ -2,6 +2,6 @@
  * slide-over controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::slide-over.slide-over');
