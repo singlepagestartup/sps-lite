6627655563733fd247db027c5ee9fcae161658bd
flakecode <flakecode@yandex.ru>
2023-07-01 19:48:46 +0000

    dockerfile for different OS

--- frontend/static.sh
+++ frontend/static.sh
@@ -9,13 +9,18 @@ npm run build:icp
 # GET https://7jwa3-hqaaa-aaaao-aanuq-cai.icp0.io/_next/static/chunks/app/%5Blocale%5D/%5B%5B...url%5D%5D/page-eed475f3356c9ad1.js net::ERR_ABORTED 502
 # import should be
 # https://7jwa3-hqaaa-aaaao-aanuq-cai.icp0.io/_next/static/chunks/app/[locale]/[[...url]]/page-eed475f3356c9ad1.js
-file=$(find out/_next/static/chunks -name "webpack-*.js" -type f)
+file=$(find ./out/_next/static/chunks -name "webpack-*.js" -type f)
 echo $file
 
 # add regex, because 's' and 'e' in s.tu(e) can be any string
 old_function="l.tu(e)"
 new_function="decodeURI($old_function)"
-sed -i '' -e "s/$old_function/$new_function/gi" $file
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e "s/$old_function/$new_function/gi" $file
+else
+  sed -i -e "s/$old_function/$new_function/gi" $file
+fi
 
 # Moving files from out/en/ folder to out/
 mv out/en/* out/
