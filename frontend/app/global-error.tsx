45ca1cce2958014c65e9ce42832fc433823a168a
rogwild <rogwild.design@gmail.com>
2023-07-26 20:22:27 +0300

    removed react hoork

--- frontend/app/global-error.tsx
+++ frontend/app/global-error.tsx
@@ -7,7 +7,7 @@ import { pagePopulate } from "~utils/api/queries";
 import { BACKEND_URL } from "~utils/envs";
 import { fonts } from "./fonts";
 
-export default async function GlobalError({ error, reset }: any) {
+export default function GlobalError({ error, reset }: any) {
   const [page, setPage] = useState<any>();
   useEffect(() => {
     console.log("GlobalError error:", error);
