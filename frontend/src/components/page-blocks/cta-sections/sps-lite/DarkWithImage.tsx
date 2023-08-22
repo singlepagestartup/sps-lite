785a95d8d492f65ddbd3a4f4f68354dd23a93770
rogwild <rogwild.design@gmail.com>
2023-08-22 22:45:06 +0700

    fixed react markdown

--- frontend/src/components/page-blocks/cta-sections/sps-lite/DarkWithImage.tsx
+++ frontend/src/components/page-blocks/cta-sections/sps-lite/DarkWithImage.tsx
@@ -29,16 +29,16 @@ export default function DarkWithImage(props: ISpsLiteCtaSectionsBlock) {
         <div className="relative isolate grid grid-rows-2 lg:grid-rows-1 grid-cols-4 gap-4 overflow-hidden px-6 pt-16 bg-gray-900 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:px-24 lg:pt-0">
           <div className="absolute top-1/2 left-1/2 -z-10 h-[80vw] w-[80vw] -translate-y-1/2 lg:left-1/2 lg:ml-0 lg:translate-y-0 lg:-translate-x-1/2 rounded-full bg-gradient-radial from-primary-600 to-transparent [mask-image:radial-gradient(closest-side,white,transparent)]"></div>
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left col-span-4 lg:col-span-2">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              {props.title ? (
-                <ReactMarkdown>{props.title}</ReactMarkdown>
-              ) : null}
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              {props?.description ? (
-                <ReactMarkdown>{props?.description}</ReactMarkdown>
-              ) : null}
-            </p>
+            {props.title ? (
+              <ReactMarkdown className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                {props.title}
+              </ReactMarkdown>
+            ) : null}
+            {props?.description ? (
+              <ReactMarkdown className="mt-6 text-lg leading-8 text-gray-300">
+                {props?.description}
+              </ReactMarkdown>
+            ) : null}
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               {props?.buttons?.map((button, index) => {
                 return <Buttons key={index} {...button} />;
