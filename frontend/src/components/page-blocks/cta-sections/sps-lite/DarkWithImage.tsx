a3900e9152551ab1aab19f15774a154fae4b1eda
rogwild <rogwild.design@gmail.com>
2023-09-18 15:30:40 +0700

    Update DarkWithImage.tsx

--- frontend/src/components/page-blocks/cta-sections/sps-lite/DarkWithImage.tsx
+++ frontend/src/components/page-blocks/cta-sections/sps-lite/DarkWithImage.tsx
@@ -8,7 +8,6 @@ import getFileUrl from "~utils/api/get-file-url";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 export default function DarkWithImage(props: ISpsLiteCtaSectionsBlock) {
-  console.log("🚀 ~ DarkWithImage ~ props:", props);
   const additionalAttributes = useMemo(() => {
     if (props?.anchor) {
       return {
