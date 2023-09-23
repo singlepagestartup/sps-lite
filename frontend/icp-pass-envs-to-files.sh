e6b27a3aec9490bc3a8cdadb34927428cfa1b001
rogwild <rogwild.design@gmail.com>
2023-09-23 19:29:43 +0700

    dots

--- frontend/icp-pass-envs-to-files.sh
+++ frontend/icp-pass-envs-to-files.sh
@@ -7,6 +7,3 @@ echo $ICP_DEPLOYER_WALLET > sps-lite-deployer-wallet.txt
 # Import identity to the file
 touch sps-lite-deployer.pem
 echo $ICP_DEPLOYER_KEY > sps-lite-deployer.pem
-
-cat sps-lite-deployer-wallet.txt
-cat sps-lite-deployer.pem
