b291bda8b48f8f18cfbd0d83bbd51d0933340a73
rogwild <rogwild.design@gmail.com>
2023-09-23 19:24:47 +0700

    create files

--- frontend/icp-pass-envs-to-files.sh
+++ frontend/icp-pass-envs-to-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Import wallet to the file
+touch sps-lite-deployer-wallet.txt
 echo $ICP_DEPLOYER_WALLET > sps-lite-deployer-wallet.txt
 
 # Import identity to the file
+touch sps-lite-deployer.pem
 echo $ICP_DEPLOYER_KEY > sps-lite-deployer.pem
