3ac879533a2356e9bcb49ac9578e5c56d65e00d7
rogwild <rogwild.design@gmail.com>
2023-11-14 23:14:16 +0300

    Update AWS S3 bucket name and region in Ansible deployment

--- deployment/ansible/aws.sh
+++ deployment/ansible/aws.sh
@@ -5,15 +5,21 @@ PROJECT_NAME=$(get_env PROJECT_NAME)
 
 AWS_ACCESS_KEY=$(get_env ROOT_AWS_ACCESS_KEY)
 AWS_SECRET_ACCESS_KEY=$(get_env ROOT_AWS_SECRET_ACCESS_KEY)
-AWS_S3_REGION=eu-central-1
-AWS_S3_BUCKET_NAME=$(get_env AWS_S3_BUCKET_NAME)
+
+AWS_S3_REGION=$(get_env AWS_S3_REGION)
+AWS_S3_BUCKET=$(get_env AWS_S3_BUCKET)
 
 GITHUB_TOKEN=$(get_env GITHUB_TOKEN)
 GITHUB_REPOSITORY=$(get_env GITHUB_REPOSITORY)
 
-if [ -z "$AWS_S3_BUCKET_NAME" ]
+if [ -z "$AWS_S3_BUCKET" ]
 then
-    AWS_S3_BUCKET_NAME=$PROJECT_NAME
+    AWS_S3_BUCKET=$PROJECT_NAME
+fi
+
+if [ -z "$AWS_S3_REGION" ]
+then
+    AWS_S3_REGION=eu-central-1
 fi
 
 if [ "$1" != "down" ]
@@ -23,21 +29,21 @@ then
         -e "AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
             AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
             PROJECT_NAME=$PROJECT_NAME \
-            AWS_S3_BUCKET_NAME=$AWS_S3_BUCKET_NAME" && \
+            AWS_S3_BUCKET=$AWS_S3_BUCKET" && \
     ansible-playbook \
         ./aws/create_s3.yaml \
         -e "AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
             AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
             AWS_S3_REGION=$AWS_S3_REGION \
             PROJECT_NAME=$PROJECT_NAME \
-            AWS_S3_BUCKET_NAME=$AWS_S3_BUCKET_NAME" && \
+            AWS_S3_BUCKET=$AWS_S3_BUCKET" && \
     ansible-playbook \
         ./aws/fill_github.yaml \
         -e "GITHUB_TOKEN=$GITHUB_TOKEN \
             GITHUB_REPOSITORY=$GITHUB_REPOSITORY \
             AWS_S3_REGION=$AWS_S3_REGION \
             PROJECT_NAME=$PROJECT_NAME \
-            AWS_S3_BUCKET_NAME=$AWS_S3_BUCKET_NAME"
+            AWS_S3_BUCKET=$AWS_S3_BUCKET"
 else
     ansible-playbook \
         ./aws/clear_github.yaml \
