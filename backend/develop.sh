dc087a9cc5fc84be16fee324c64d55ec117f6e17
rogwild <rogwild.design@gmail.com>
2024-01-06 13:11:16 +0300

    command for run plugins build

--- backend/develop.sh
+++ backend/develop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+find ./src/plugins -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && npm install && npm run build" \;
 npm rebuild --verbose sharp
 npm run develop
--- backend/up.sh
+++ backend/up.sh
@@ -2,4 +2,5 @@
 chmod +x ./create_env.sh ./develop.sh
 ./create_env.sh
 npm install
+find ./src/plugins -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && npm install && npm run build" \;
 ./develop.sh
