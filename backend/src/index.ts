c89a8834f88f27c98d98db4c41c868e5e997330c
rogwild <rogwild.design@gmail.com>
2024-01-12 03:43:21 +0300

    run seeder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,6 @@ import path from "path";
 import setPermissions from "./utils/bootstrap/set-permissions";
 import clearMediaLibrary from "./utils/bootstrap/clear-media-library";
 import Telegram from "./services/Telegram";
-import fs from "fs/promises";
-import seeder from "./utils/seeder";
 import dumper from "./utils/dumper";
 
 export default {
@@ -24,31 +22,30 @@ export default {
       clearMediaLibrary();
     }
 
-    // const apiPath = path.join(__dirname, "../../src/", "./api");
-    await strapi.service("plugin::sps-migrate.seeder").run();
-
     if (process.env.SEED_ENTITES) {
-      const lockFilePath = path.join(__dirname, "../../", "seeded.txt");
-      try {
-        await fs.rm(lockFilePath);
-      } catch (error) {
-        //
-      }
+      await strapi.service("plugin::sps-migrate.seeder").run();
 
-      try {
-        const apiPath = path.join(__dirname, "../../src/", "./api");
-        seeder(apiPath).then(async () => {
-          await fs.writeFile(lockFilePath, "");
+      // const lockFilePath = path.join(__dirname, "../../", "seeded.txt");
+      // try {
+      //   await fs.rm(lockFilePath);
+      // } catch (error) {
+      //   //
+      // }
 
-          if (process.env.CLEAR_MEDIA_LIBRARY) {
-            clearMediaLibrary();
-          }
-        });
-      } catch (error) {
-        console.log("🚀 ~ bootstrap ~ seeder ~ error: ", error.message);
+      // try {
+      //   const apiPath = path.join(__dirname, "../../src/", "./api");
+      //   seeder(apiPath).then(async () => {
+      //     await fs.writeFile(lockFilePath, "");
 
-        strapi.errorCatcher(error);
-      }
+      //     if (process.env.CLEAR_MEDIA_LIBRARY) {
+      //       clearMediaLibrary();
+      //     }
+      //   });
+      // } catch (error) {
+      //   console.log("🚀 ~ bootstrap ~ seeder ~ error: ", error.message);
+
+      //   strapi.errorCatcher(error);
+      // }
     }
 
     if (process.env.RUN_TELEGRAM_BOT) {
