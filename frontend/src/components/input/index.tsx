334907cc5bb9f73da6e09948f7eaae749bd23d60
rogwild <rogwild.design@gmail.com>
2024-01-15 16:39:16 +0300

    inputs refactoring

--- frontend/src/components/input/index.tsx
+++ frontend/src/components/input/index.tsx
@@ -1,64 +1,15 @@
 "use client";
 
-import { FC, useMemo } from "react";
-import { UseControllerProps } from "react-hook-form";
+import { useMemo } from "react";
 import FileInput from "./file";
 import ListboxInput from "./listbox";
 import RadioGroupInput from "./radio-group";
 import RangeInput from "./range";
-import RepeatableInput, { IInsideComponentProps } from "./repeatable";
+import RepeatableInput from "./repeatable";
 import SwitchInput from "./switch";
 import TextInput from "./text";
 import DateInput from "./date";
-import { IEntity as IBackendFile } from "~redux/services/backend/extensions/upload/api/file/interfaces";
-
-export interface IInputProps extends UseControllerProps {
-  label?: string | null;
-  options?: any;
-  ButtonComp?: any;
-  index?: number;
-  OptionComp?: any;
-  placeholder?: string | null;
-  initialValue?: any;
-  multiple?: boolean | null;
-  accept?: string;
-  by?: string;
-  className?: string;
-  inputConfig?: any;
-  parentKey?: string;
-  defaultValue?: any;
-  baseKey?: string;
-  inputs?: any;
-  rules?: any;
-  type?: string | null;
-  rows?: number;
-  removeButtonTitle?: string;
-  addButtonTitle?: string;
-  translate?: (message: string) => string;
-  renderOptionValue?: (option: any) => string;
-  valueAsNumber?: boolean;
-  InsideComponent?: FC<IInsideComponentProps>;
-  step?: number | null;
-  min?: number | null;
-  max?: number | null;
-  disabled?: boolean;
-  media?: IBackendFile[] | null;
-  additionalMedia?: IBackendFile[] | null;
-  extraMedia?: IBackendFile[] | null;
-  ResetIcon?: any;
-  CalendarIcon?: any;
-  onAppend?: ({ fieldIndex }: { fieldIndex: number }) => any;
-  onRemove?: ({ fieldIndex }: { fieldIndex: number }) => any;
-  variant:
-    | "text"
-    | "listbox"
-    | "radio-group"
-    | "switch"
-    | "file"
-    | "repeatable"
-    | "range"
-    | "date";
-}
+import { IInputProps } from "~components/ui/input";
 
 export default function Input(props: IInputProps) {
   const { variant } = props;
---
+++ frontend/src/components/ui/input/index.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+import { IEntity as IBackendFile } from "~redux/services/backend/extensions/upload/api/file/interfaces";
+import { UseControllerProps } from "react-hook-form";
+import { IInsideComponentProps } from "./repeatable";
+
+export interface IInputProps extends UseControllerProps {
+  label?: string | null;
+  options?: any;
+  ButtonComp?: any;
+  index?: number;
+  OptionComp?: any;
+  placeholder?: string | null;
+  initialValue?: any;
+  multiple?: boolean | null;
+  accept?: string;
+  by?: string;
+  className?: string;
+  inputConfig?: any;
+  parentKey?: string;
+  defaultValue?: any;
+  baseKey?: string;
+  inputs?: any;
+  rules?: any;
+  type?: string | null;
+  rows?: number;
+  removeButtonTitle?: string;
+  addButtonTitle?: string;
+  translate?: (message: string) => string;
+  renderOptionValue?: (option: any) => string;
+  valueAsNumber?: boolean;
+  InsideComponent?: FC<IInsideComponentProps>;
+  step?: number | null;
+  min?: number | null;
+  max?: number | null;
+  disabled?: boolean;
+  media?: IBackendFile[] | null;
+  additionalMedia?: IBackendFile[] | null;
+  extraMedia?: IBackendFile[] | null;
+  ResetIcon?: any;
+  CalendarIcon?: any;
+  onAppend?: ({ fieldIndex }: { fieldIndex: number }) => any;
+  onRemove?: ({ fieldIndex }: { fieldIndex: number }) => any;
+  variant:
+    | "text"
+    | "listbox"
+    | "radio-group"
+    | "switch"
+    | "file"
+    | "repeatable"
+    | "range"
+    | "date";
+}
