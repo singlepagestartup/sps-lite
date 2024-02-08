ab00c5fda80950000ca349f5c5317f02d9ea9303
rogwild <rogwild.design@gmail.com>
2024-02-09 01:45:51 +0300

    frontend deployment script

--- libs/modules/sps-website-builder/frontend/src/lib/app/root-layout.tsx
+++ libs/modules/sps-website-builder/frontend/src/lib/app/root-layout.tsx
@@ -2,15 +2,12 @@
 import "server-only";
 
 import { Suspense } from "react";
-import { ReduxProvider } from "../redux";
 
 export async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <section data-app="sps-website-builder" className="relative">
       {/* Suspense here is for static build, without that build will return nothing */}
-      {/* <ReduxProvider> */}
       <Suspense>{children}</Suspense>
-      {/* </ReduxProvider> */}
     </section>
   );
 }
--- libs/modules/sps-website-builder/frontend/src/lib/components/page-block/aliases/sps-lite.ts
+++ libs/modules/sps-website-builder/frontend/src/lib/components/page-block/aliases/sps-lite.ts
@@ -1,49 +1,49 @@
+import { Component as AlertBlock } from "../../../models/alert-block/component";
+import { Component as Button } from "../../../models/button/component";
+import { Component as ButtonsArray } from "../../../models/buttons-array/component";
+import { Component as CheckoutFormBlock } from "../../../models/checkout-form-block/component";
+import { Component as ContactSectonBlock } from "../../../models/contact-section-block/component";
+import { Component as CtaSectionBlock } from "../../../models/cta-section-block/component";
+import { Component as EditSubscriptionBlock } from "../../../models/edit-subscription-block/component";
 import { Component as FaqBlock } from "../../../models/faq-block/component";
 import { Component as FeaturesSectionBlock } from "../../../models/features-section-block/component";
 import { Component as FooterBlock } from "../../../models/footer-block/component";
 import { Component as HeaderSectionBlock } from "../../../models/header-section-block/component";
 import { Component as HeroSectionBlock } from "../../../models/hero-section-block/component";
-import { Component as LogotypesCloudBlock } from "../../../models/logotypes-cloud-block/component";
-import { Component as NotFoundBlock } from "../../../models/not-found-block/component";
 import { Component as IncentivesBlock } from "../../../models/incentives-block/component";
-import { Component as CtaSectionBlock } from "../../../models/cta-section-block/component";
-import { Component as AlertBlock } from "../../../models/alert-block/component";
-import { Component as SliderBlock } from "../../../models/slider-block/component";
+import { Component as LogotypesCloudBlock } from "../../../models/logotypes-cloud-block/component";
 import { Component as NavbarBlock } from "../../../models/navbar-block/component";
-import { Component as Button } from "../../../models/button/component";
-import { Component as ButtonsArray } from "../../../models/buttons-array/component";
-import { Component as CheckoutFormBlock } from "../../../models/checkout-form-block/component";
+import { Component as NotFoundBlock } from "../../../models/not-found-block/component";
 import { Component as ProductsListBlock } from "../../../models/products-list-block/component";
-import { Component as ShoppingCartBlock } from "../../../models/shopping-cart-block/component";
-import { Component as ContactSectonBlock } from "../../../models/contact-section-block/component";
 import { Component as ReviewsListBlock } from "../../../models/reviews-list-block/component";
 import { Component as ReviewsTableBlock } from "../../../models/reviews-table-block/component";
-import { Component as EditSubscriptionBlock } from "../../../models/edit-subscription-block/component";
-import { Component as TiersListBlock } from "../../../models/tiers-list-block/component";
+import { Component as ShoppingCartBlock } from "../../../models/shopping-cart-block/component";
+import { Component as SliderBlock } from "../../../models/slider-block/component";
 import { Component as SubscriptionCheckoutFormBlock } from "../../../models/subscription-checkout-form-block/component";
+import { Component as TiersListBlock } from "../../../models/tiers-list-block/component";
 
 export const pageBlocks = {
+  "page-blocks.alert-block": AlertBlock,
   "elements.buttons-array": ButtonsArray,
   "elements.button": Button,
   "page-blocks.checkout-form-block": CheckoutFormBlock,
-  "page-blocks.products-list-block": ProductsListBlock,
-  "page-blocks.shopping-cart-block": ShoppingCartBlock,
   "page-blocks.contact-section-block": ContactSectonBlock,
-  "page-blocks.reviews-list-block": ReviewsListBlock,
-  "page-blocks.reviews-table-block": ReviewsTableBlock,
-  "page-blocks.tiers-list-block": TiersListBlock,
+  "page-blocks.cta-section-block": CtaSectionBlock,
   "page-blocks.edit-subscription-block": EditSubscriptionBlock,
-  "page-blocks.subscription-checkout-form-block": SubscriptionCheckoutFormBlock,
+  "page-blocks.faq-block": FaqBlock,
+  "page-blocks.features-section-block": FeaturesSectionBlock,
   "page-blocks.footer-block": FooterBlock,
   "page-blocks.header-section-block": HeaderSectionBlock,
   "page-blocks.hero-section-block": HeroSectionBlock,
-  "page-blocks.features-section-block": FeaturesSectionBlock,
-  "page-blocks.faqs-block": FaqBlock,
-  "page-blocks.logotypes-cloud-block": LogotypesCloudBlock,
-  "page-blocks.slider-block": SliderBlock,
-  "page-blocks.not-found-block": NotFoundBlock,
   "page-blocks.incentives-block": IncentivesBlock,
-  "page-blocks.cta-section-block": CtaSectionBlock,
-  "page-blocks.alert-block": AlertBlock,
+  "page-blocks.logotypes-cloud-block": LogotypesCloudBlock,
   "page-blocks.navbar-block": NavbarBlock,
+  "page-blocks.not-found-block": NotFoundBlock,
+  "page-blocks.products-list-block": ProductsListBlock,
+  "page-blocks.reviews-list-block": ReviewsListBlock,
+  "page-blocks.reviews-table-block": ReviewsTableBlock,
+  "page-blocks.slider-block": SliderBlock,
+  "page-blocks.shopping-cart-block": ShoppingCartBlock,
+  "page-blocks.subscription-checkout-form-block": SubscriptionCheckoutFormBlock,
+  "page-blocks.tiers-list-block": TiersListBlock,
 };
