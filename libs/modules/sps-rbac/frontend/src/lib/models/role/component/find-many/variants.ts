5c06d154804e9ec4f8b001e9b70869da27074450
rogwild <rogwild.design@gmail.com>
2024-02-16 21:38:30 +0300

    sps rbac

--- libs/modules/sps-rbac/frontend/src/lib/models/role/component/find-many/variants.ts
+++
@@ -1,7 +0,0 @@
-import { variants as spsLiteVariants } from "./sps-lite/variants";
-import { variants as startupVariants } from "./startup/variants";
-
-export const variants = {
-  ...spsLiteVariants,
-  ...startupVariants,
-};
