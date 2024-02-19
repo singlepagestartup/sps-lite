db5ba6a92af12c4c5c17807e546143f73c65aee0
rogwild <rogwild.design@gmail.com>
2024-02-19 16:34:58 +0300

    move variants

--- move_variants.sh
+++ move_variants.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 module=sps-crm
 model_name=review
-variants=()
 
 npx nx generate @nx/workspace:move --project=@sps/$module-$model_name-component --destination=libs/modules/$module/models/$model_name/component/root
 
-# for variant in ${variants[@]}; do
-#     npx nx g @nx/react:library --name=@sps/$module-$model_name-component-variants-sps-lite-$variant --dir=libs/modules/$module/models/$model_name/component/variants/sps-lite/$variant --bundler=none --compiler=babel --style=none --minimal=true --component=false
-# done
+npx nx g @nx/react:library --name=@sps/$module-$model_name-component-redux --dir=libs/modules/$module/models/$model_name/component/redux --bundler=none --compiler=babel --style=none --minimal=true --component=false
 
-# npx nx g @nx/react:library --name=@sps/$module-$model_name-component-redux --dir=libs/modules/$module/models/$model_name/component/redux --bundler=none --compiler=babel --style=none --minimal=true --component=false
+# move contracts
+npx nx generate @nx/workspace:move --project=@sps/$module-$model_name-contracts --destination=libs/modules/$module/models/$model_name/contracts/root
+npx nx generate @nx/workspace:move --project=@sps/$module-$model_name-contracts-extended --destination=libs/modules/$module/models/$model_name/contracts/extended
 
-# # move contracts
-# npx nx generate @nx/workspace:move --project=@sps/$module-$model_name-contracts --destination=libs/modules/$module/models/$model_name/contracts/root
-# npx nx generate @nx/workspace:move --project=@sps/$module-$model_name-contracts-extended --destination=libs/modules/$module/models/$model_name/contracts/extended
+mkdir -p libs/modules/$module/models/$model_name/component/variants/startup
+touch libs/modules/$module/models/$model_name/component/variants/startup/.gitkeep
 
-# mkdir -p libs/modules/$module/models/$model_name/component/variants/startup
-# touch libs/modules/$module/models/$model_name/component/variants/startup/.gitkeep
+# copy file
+mkdir -p libs/modules/$module/models/$model_name/component/redux/src/lib
+mv libs/modules/$module/models/$model_name/component/root/src/lib/redux.tsx libs/modules/$module/models/$model_name/component/redux/src/lib/index.tsx
 
-# # copy file
-# mkdir -p libs/modules/$module/models/$model_name/component/redux/src/lib
-# mv libs/modules/$module/models/$model_name/component/root/src/lib/redux.tsx libs/modules/$module/models/$model_name/component/redux/src/lib/index.tsx
-
-# # write to file
-# echo "export { ReduxProvider } from './lib';" > libs/modules/$module/models/$model_name/component/redux/src/index.ts
+# write to file
+echo "export { ReduxProvider } from './lib';" > libs/modules/$module/models/$model_name/component/redux/src/index.ts
 
 # read folders names
 for folder in $(ls -d libs/modules/$module/models/$model_name/component/root/src/lib/component/sps-lite/*); do
