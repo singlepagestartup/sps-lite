1ec5fdf5fe71d32abf3070ec4f8649920cc9a01c
rogwild <rogwild.design@gmail.com>
2024-02-20 22:30:08 +0300

    test lintstaged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  "**/*.{js,ts,jsx,tsx}": () => {
-    return `nx affected:lint`;
-  },
+  "{apps,libs,tools}/**/*.{js,jsx,ts,tsx}": [
+    (files) => {
+      return `nx format:write --files=${files.join(",")}`;
+    },
+    (files) => {
+      return `nx affected:lint --files=${files.join(",")}`;
+    },
+  ],
+  "{apps,libs,tools}/**/*.{json}": [
+    (files) => {
+      return `nx format:write --files=${files.join(",")}`;
+    },
+  ],
 };
