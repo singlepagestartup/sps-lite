b215424e46ecdea99ade7ccb971e428cc1da6a7b
rogwild <rogwild.design@gmail.com>
2024-02-21 01:50:51 +0300

    moved ecommerce order contracts

--- libs/modules/sps-ecommerce/models/order/contracts/root/jest.config.ts
+++ libs/modules/sps-ecommerce/models/order/contracts/root/jest.config.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export default {
-  displayName: "@sps/sps-ecommerce-order-contracts-extended",
-  preset: "../../../../../../jest.preset.js",
+  displayName: "@sps/sps-ecommerce-order-contracts",
+  preset: "../../../../../../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
   coverageDirectory:
-    "../../../../../../coverage/libs/modules/sps-ecommerce/models/order/contracts-extended",
+    "../../../../../../../coverage/libs/modules/sps-ecommerce/models/order/contracts/root",
 };
