1dee6868396a0bff29a3b299e08dae6944d8ec4d
rogwild <rogwild.design@gmail.com>
2024-02-21 16:48:24 +0300

    renamed hero section

--- libs/modules/sps-website-builder/models/hero-section-block/frontend/component/root/src/lib/sps-lite/variants.ts
+++ libs/modules/sps-website-builder/models/hero-section-block/frontend/component/root/src/lib/sps-lite/variants.ts
@@ -1,4 +1,4 @@
-import { Component as SimpleCentered } from "@sps/sps-website-builder-hero-section-block-component-variants-sps-lite-simple-centered";
+import { Component as SimpleCentered } from "@sps/sps-website-builder-hero-section-block-frontend-component-variants-sps-lite-simple-centered";
 
 export const variants = {
   "simple-centered": SimpleCentered,
--- move_components.sh
+++ move_components.sh
@@ -1,49 +1,64 @@
 #!/bin/bash
 module=sps-website-builder
-model_names=(alert-block button buttons-array checkout-form-block contact-section-block cta-section-block edit-subscription-block faq faq-block feature features-section-block flyout font footer footer-block header-section-block hero-section-block incentives-block layout loader locale logotype logotypes-cloud-block metatag modal navbar navbar-block not-found-block page products-list-block reviews-list-block reviews-table-block shopping-cart-block sidebar slide slide-over slider slider-block subscription-checkout-form-block theme tiers-list-block topbar)
+# model_names=(alert-block button buttons-array checkout-form-block contact-section-block cta-section-block edit-subscription-block faq faq-block feature features-section-block flyout font footer footer-block header-section-block hero-section-block incentives-block layout loader locale logotype logotypes-cloud-block metatag modal navbar navbar-block not-found-block page products-list-block reviews-list-block reviews-table-block shopping-cart-block sidebar slide slide-over slider slider-block subscription-checkout-form-block theme tiers-list-block topbar)
 
-for model_name in "${model_names[@]}"
-do
-    echo $model_name
-    # model_name=attribute
-    variants=()
+# for model_name in "${model_names[@]}"
+# do
+#     echo $model_name
+#     # model_name=attribute
+#     variants=()
 
-    # read folders names
-    for folder in $(ls -d libs/modules/$module/models/$model_name/component/variants/sps-lite/*); do
-        # check is directory
-        if [ -d $folder ]; then
-            # get last string
-            folder=${folder##*/}
-            # add to variants
-            variants+=($folder)
-        fi
-    done
+#     # read folders names
+#     for folder in $(ls -d libs/modules/$module/models/$model_name/component/variants/sps-lite/*); do
+#         # check is directory
+#         if [ -d $folder ]; then
+#             # get last string
+#             folder=${folder##*/}
+#             # add to variants
+#             variants+=($folder)
+#         fi
+#     done
 
-    # echo variants: ${variants[@]}
+#     # echo variants: ${variants[@]}
 
-    nx reset
+#     nx reset
 
-    npx nx g @nx/workspace:move --project=@sps/$module-$model_name-api --destination=libs/modules/$module/models/$model_name/frontend/api --newProjectName=@sps/$module-$model_name-frontend-api
+#     npx nx g @nx/workspace:move --project=@sps/$module-$model_name-api --destination=libs/modules/$module/models/$model_name/frontend/api --newProjectName=@sps/$module-$model_name-frontend-api
 
-    nx reset
+#     nx reset
 
-    npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component-redux --destination=libs/modules/$module/models/$model_name/frontend/redux --newProjectName=@sps/$module-$model_name-frontend-redux
+#     npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component-redux --destination=libs/modules/$module/models/$model_name/frontend/redux --newProjectName=@sps/$module-$model_name-frontend-redux
 
 
-    for variant in "${variants[@]}"
-    do
-        nx reset
-        npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component-variants-sps-lite-$variant --destination=libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/$variant --newProjectName=@sps/$module-$model_name-frontend-component-sps-lite-variants-$variant
-    done
+#     for variant in "${variants[@]}"
+#     do
+#         nx reset
+#         npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component-variants-sps-lite-$variant --destination=libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/$variant --newProjectName=@sps/$module-$model_name-frontend-component-sps-lite-variants-$variant
+#     done
 
-    nx reset
+#     nx reset
 
-    npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component --destination=libs/modules/$module/models/$model_name/frontend/component/root --newProjectName=@sps/$module-$model_name-frontend-component
+#     npx nx g @nx/workspace:move --project=@sps/$module-$model_name-component --destination=libs/modules/$module/models/$model_name/frontend/component/root --newProjectName=@sps/$module-$model_name-frontend-component
 
-    # move startup variants
-    mv libs/modules/$module/models/$model_name/component/variants/startup libs/modules/$module/models/$model_name/frontend/component/variants/startup
+#     # move startup variants
+#     mv libs/modules/$module/models/$model_name/component/variants/startup libs/modules/$module/models/$model_name/frontend/component/variants/startup
 
-    # remove old folders
-    rm -rf libs/modules/$module/models/$model_name/component/variants
-    rm -rf libs/modules/$module/models/$model_name/component
-done
+#     # remove old folders
+#     rm -rf libs/modules/$module/models/$model_name/component/variants
+#     rm -rf libs/modules/$module/models/$model_name/component
+# done
+
+# model_name=hero-section-block
+# variant=simple-centered
+
+# nx reset
+
+# npx nx g @nx/workspace:move --project @sps/$module-$model_name-component-variants-sps-lite-$variant --destination libs/modules/$module/models/$model_name/frontend/component/variants/ptmp/$variant --newProjectName @sps/$module-$model_name-frontend-component-variants-sps-lite-$variant
+
+# nx reset
+
+# npx nx g @nx/workspace:move --project=@sps/$module-$model_name-frontend-component-variants-sps-lite-$variant --destination=libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/$variant --newProjectName=@sps/$module-$model_name-frontend-component-variants-sps-lite-$variant
+
+# replace "@sps/$module-$model_name-component-variants-sps-lite-$variant" to "sps/$module-$model_name-frontend-component-variants-sps-lite-$variant" in tsconfig.base.json
+
+# sed -i '' "s/@sps\/$module-$model_name-component-variants-sps-lite-$variant/@sps\/$module-$model_name-frontend-component-variants-sps-lite-$variant/g" tsconfig.base.json
