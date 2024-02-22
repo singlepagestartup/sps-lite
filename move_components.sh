eadc378ad8b2841d0db8a35c89cb19f5e9ea3416
rogwild <rogwild.design@gmail.com>
2024-02-22 12:57:46 +0300

    rename contracts

--- move_components.sh
+++ move_components.sh
@@ -4,37 +4,46 @@ model_names=(currency invoice)
 
 for model_name in "${model_names[@]}"
 do
-    # echo $model_name
-    # model_name=attribute
-    variants=()
+    # rename contracts projects
+    rm -rf libs/modules/$module/models/$model_name/contracts/tmp-root
 
-    # read folders names
-    for folder in $(ls -d libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/*); do
-        # check is directory
-        if [ -d $folder ]; then
-            # get last string
-            folder=${folder##*/}
-            # add to variants
-            variants+=($folder)
-        fi
-    done
+    npx nx g @nx/workspace:move --project @sps/$module-$model_name-contracts --destination libs/modules/$module/models/$model_name/contracts/tmp-root --newProjectName @sps/$module-models-$model_name-contracts
+    
+    npx nx g @nx/workspace:move --project @sps/$module-models-$model_name-contracts --destination libs/modules/$module/models/$model_name/contracts/root
 
-    # echo variants: ${variants[@]}
+    # rm -rf libs/modules/$module/models/$model_name/contracts/tmp-extended
+
+    # # echo $model_name
+    # # model_name=attribute
+    # variants=()
+
+    # # read folders names
+    # for folder in $(ls -d libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/*); do
+    #     # check is directory
+    #     if [ -d $folder ]; then
+    #         # get last string
+    #         folder=${folder##*/}
+    #         # add to variants
+    #         variants+=($folder)
+    #     fi
+    # done
+
+    # # echo variants: ${variants[@]}
 
 
-    for variant in "${variants[@]}"
-    do
-        rm -rf libs/modules/$module/models/$model_name/frontend/component/variants/tmp-$variant
+    # for variant in "${variants[@]}"
+    # do
+    #     rm -rf libs/modules/$module/models/$model_name/frontend/component/variants/tmp-$variant
 
-        # nx reset
-        echo $variant
+    #     # nx reset
+    #     echo $variant
 
-        nx reset
+    #     nx reset
 
-        npx nx g @nx/workspace:move --project @sps/$module-$model_name-frontend-component-sps-lite-variants-$variant --destination libs/modules/$module/models/$model_name/frontend/component/variants/tmp-$variant --newProjectName @sps/$module-models-$model_name-frontend-component-variants-sps-lite-$variant
+    #     npx nx g @nx/workspace:move --project @sps/$module-$model_name-frontend-component-sps-lite-variants-$variant --destination libs/modules/$module/models/$model_name/frontend/component/variants/tmp-$variant --newProjectName @sps/$module-models-$model_name-frontend-component-variants-sps-lite-$variant
 
-        nx reset
+    #     nx reset
 
-        npx nx g @nx/workspace:move --project @sps/$module-models-$model_name-frontend-component-variants-sps-lite-$variant --destination libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/$variant
-    done
+    #     npx nx g @nx/workspace:move --project @sps/$module-models-$model_name-frontend-component-variants-sps-lite-$variant --destination libs/modules/$module/models/$model_name/frontend/component/variants/sps-lite/$variant
+    # done
 done
