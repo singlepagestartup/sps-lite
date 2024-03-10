0077268a88e21595bccee226faa68f286cf27691
rogwild <rogwild.design@gmail.com>
2024-03-10 12:22:38 +0300

    create fetch

--- libs/shared/frontend/utils/client/src/lib/fetch-api.ts
+++ libs/shared/frontend/utils/client/src/lib/fetch-api.ts
@@ -1,4 +1,5 @@
 import { BACKEND_URL } from "./envs";
+import { prepareFormDataToSend } from "./preapare-form-data-to-send";
 import { transformResponseItem } from "./transform-response-item";
 import QueryString from "qs";
 
@@ -99,8 +100,44 @@ async function find<T>(params: {
   return transformedData;
 }
 
+async function create<T>(params: {
+  model: string;
+  populate: any;
+  rootPath?: string;
+  data: any;
+}): Promise<T[]> {
+  const { populate, model, rootPath = "/api/sps-website-builder" } = params;
+
+  const stringifiedQuery = QueryString.stringify(
+    {
+      populate,
+    },
+    {
+      encodeValuesOnly: true,
+    },
+  );
+
+  const formData = prepareFormDataToSend(params.data);
+
+  const res = await fetch(
+    `${BACKEND_URL}${rootPath}/${model}?${stringifiedQuery}`,
+    { next: { revalidate: 3600 }, method: "POST", body: formData } as any,
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const json = await res.json();
+
+  const transformedData = transformResponseItem(json);
+
+  return transformedData;
+}
+
 export const api = {
   findByIdAndName,
   findOne,
   find,
+  create,
 };
