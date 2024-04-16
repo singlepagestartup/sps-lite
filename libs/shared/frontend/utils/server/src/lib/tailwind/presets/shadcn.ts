b30f027ec0cc1e701c3c165c12f15df55430f2c1
rogwild <rogwild.design@gmail.com>
2024-04-16 14:36:37 +0200

    fixed padding regex

--- libs/shared/frontend/utils/server/src/lib/tailwind/presets/shadcn.ts
+++ libs/shared/frontend/utils/server/src/lib/tailwind/presets/shadcn.ts
@@ -151,7 +151,7 @@ export const preset = {
   darkMode: ["class"],
   content: [],
   safelist: [
-    { pattern: /^p(\w?)-/ },
+    { pattern: /^p\w?-/, variants: ["sm", "md", "lg", "xl", "2xl"] },
     { pattern: /w-(1|2|3|4|5|6|7|8|9|10|11)\/12/ },
     { pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
     { pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/ },
