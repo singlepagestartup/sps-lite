463bd5f18281a35ba8c2725574f06707b9f93149
rogwild <rogwild.design@gmail.com>
2024-04-19 01:34:40 +0200

    button skeletons with base data

--- libs/modules/sps-website-builder/models/button/frontend/component/variants/sps-lite/destructive/src/lib/Skeleton.tsx
+++ libs/modules/sps-website-builder/models/button/frontend/component/variants/sps-lite/destructive/src/lib/Skeleton.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@sps/ui-adapter";
 import { IComponentProps } from "./interface";
 
 export function Skeleton(props: IComponentProps) {
   return (
-    <div className={`skeleton ${props.data.className || "h-10 w-32"}`}></div>
+    <Button
+      ui="sps"
+      data-module="sps-website-builder"
+      data-model="elements.button"
+      data-variant={props.variant}
+      data-ui-variant={props.data.variant}
+      className={`${props.data.className || "w-full"}`}
+      {...(props.data.url ? { url: props.data.url } : {})}
+      {...(props.onClick ? { onClick: props.onClick } : {})}
+    >
+      <div className="button-container">{props.data.title}</div>
+    </Button>
   );
 }
