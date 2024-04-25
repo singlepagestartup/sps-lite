638ffb12b81a165c7397479b581871d3f6414c38
rogwild <rogwild.design@gmail.com>
2024-04-26 00:36:03 +0200

    verdaccio npm registry

--- strapi-plugin.sh
+++ strapi-plugin.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # https://itnext.io/local-library-development-with-nx-release-and-verdaccio-019af5c5bec0
+
+if nc -z localhost 4873; then
+    echo "Verdaccio is running"
+else
+    npx nx run @sps/source:local-registry &\
+fi
+
 npx nx run sps-billing-backend:tsc:lib && echo yes | nx release prepatch && nx release publish && npm i sps-billing-plugin@latest @sps/shared-frontend-utils-server@latest @sps/shared-utils@latest
+
+npx nx run @sps/source:force-stop-registry
