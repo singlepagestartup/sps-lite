0021b46e8c4d01a2b2171292f2c0c461302a2c4a
rogwild <rogwild.design@gmail.com>
2024-04-25 23:42:26 +0200

    removed console log

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,17 +3,10 @@
 import setPermissions from "./utils/bootstrap/set-permissions";
 import clearMediaLibrary from "./utils/bootstrap/clear-media-library";
 import Telegram from "./services/Telegram";
-import { server } from "sps-billing-plugin";
 
 export default {
   async bootstrap({ strapi }) {
     await setPermissions();
-
-    // spsBillingBackend();
-    // spsBilling.register({});
-    const cf = server.config;
-
-    console.log(`🚀 ~ bootstrap ~ cf:`, cf);
 
     strapi.errorCatcher = (error, ctx) => {
       if (process.env.SENTRY_DSN) {
