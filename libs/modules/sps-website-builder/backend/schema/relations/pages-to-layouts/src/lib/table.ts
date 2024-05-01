31bda630700f7134a2dc130ffa3c86ccd0ce8493
rogwild <rogwild.design@gmail.com>
2024-05-02 01:12:09 +0200

    relations table

--- libs/modules/sps-website-builder/backend/schema/relations/pages-to-layouts/src/lib/table.ts
+++ libs/modules/sps-website-builder/backend/schema/relations/pages-to-layouts/src/lib/table.ts
@@ -1,20 +1,27 @@
-import { pgTableCreator, primaryKey, uuid } from "drizzle-orm/pg-core";
+import pgCore from "drizzle-orm/pg-core";
 import { Table as PagesTable } from "@sps/sps-website-builder-models-page-backend-schema-table";
 import { Table as LayoutsTable } from "@sps/sps-website-builder-models-layout-backend-schema-table";
 
-const pgTable = pgTableCreator((name) => `sps_website_builder_${name}`);
+const moduleNameSnakeCased = "sps_website_builder";
+const modelNameSnakeCasedPluralized = "pages_to_layouts";
+
+const pgTable = pgCore.pgTableCreator(
+  (name) => `${moduleNameSnakeCased}_${name}`,
+);
 
 export const Table = pgTable(
-  "pages_to_layouts",
+  modelNameSnakeCasedPluralized,
   {
-    pageId: uuid("page_id")
+    pageId: pgCore
+      .uuid("page_id")
       .notNull()
       .references(() => PagesTable.id),
-    layoutId: uuid("layout_id")
+    layoutId: pgCore
+      .uuid("layout_id")
       .notNull()
       .references(() => LayoutsTable.id),
   },
   (t) => ({
-    pk: primaryKey({ columns: [t.pageId, t.layoutId] }),
+    pk: pgCore.primaryKey({ columns: [t.pageId, t.layoutId] }),
   }),
 );
