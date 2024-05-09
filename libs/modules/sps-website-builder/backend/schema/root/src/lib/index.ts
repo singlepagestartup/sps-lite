d9c24806e73b00c921f991d764915522a1a971f6
rogwild <rogwild.design@gmail.com>
2024-05-09 12:33:33 +0200

    attach and detach from module

--- libs/modules/sps-website-builder/backend/schema/root/src/lib/index.ts
+++ libs/modules/sps-website-builder/backend/schema/root/src/lib/index.ts
@@ -8,5 +8,3 @@ export {
   Relations as SPSWBPageRelations,
   VariantEnumTable as SPSWBPageVariantEnumTable,
 } from "@sps/sps-website-builder-models-page-backend-schema";
-
-export * from "@sps/sps-website-builder-backend-schema-relations";
