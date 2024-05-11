dba2e2fa2af894eaf54b55dc6d6addf168560a9f
rogwild <rogwild.design@gmail.com>
2024-05-11 19:50:19 +0200

    create backend variant generator

--- tools/plugins/sps-generator-plugin/src/generators/backend-variant/generator.ts
+++ tools/plugins/sps-generator-plugin/src/generators/backend-variant/generator.ts
@@ -2,8 +2,8 @@ import { Tree } from "@nx/devkit";
 import { BackendVariantGeneratorSchema } from "./schema";
 import { Coder } from "../../coder/Coder";
 
-// npx nx generate @sps/sps-generator-plugin:create-model --name=project --module=sps-website-builder --dry-run
-// npx nx generate @sps/sps-generator-plugin:create-model --name=portfolio --module=sps-website-builder --dry-run
+// npx nx generate @sps/sps-generator-plugin:backend-variant --variant=simple-centered --entity_name=hero-section-block --action=create --level=sps-lite --module=sps-website-builder --no-interactive --dry-run
+// npx nx generate @sps/sps-generator-plugin:backend-variant --variant=simple-centered --entity_name=hero-section-block --action=remove --level=sps-lite --module=sps-website-builder --no-interactive --dry-run
 export async function backendVariantGenerator(
   tree: Tree,
   options: BackendVariantGeneratorSchema,
