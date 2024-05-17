64c37f2da68cccb9209b2f903591584d089e5ee0
rogwild <rogwild.design@gmail.com>
2024-05-17 14:21:43 +0200

    migrations

---
+++ apps/frontend/src/db/migrations/0001_equal_nighthawk.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "sps_w_b_nr_bk" ADD COLUMN "class_name" text;
--- libs/modules/sps-website-builder/models/navbar-block/backend/schema/table/src/lib/index.ts
+++ libs/modules/sps-website-builder/models/navbar-block/backend/schema/table/src/lib/index.ts
@@ -10,6 +10,7 @@ const table = "nr_bk";
 const pgTable = pgCore.pgTableCreator((name) => `${moduleName}_${name}`);
 
 export const Table = pgTable(table, {
+  className: pgCore.text("class_name"),
   id: pgCore.uuid("id").primaryKey().defaultRandom(),
   createdAt: pgCore.timestamp("created_at").notNull().defaultNow(),
   updatedAt: pgCore.timestamp("updated_at").notNull().defaultNow(),
