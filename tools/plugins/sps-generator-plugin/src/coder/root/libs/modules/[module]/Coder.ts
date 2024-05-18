cfe21e27958753ff7f6cabc10b25ef55ce12f023
rogwild <rogwild.design@gmail.com>
2024-05-18 03:56:21 +0200

    remove relation

--- tools/plugins/sps-generator-plugin/src/coder/root/libs/modules/[module]/Coder.ts
+++ tools/plugins/sps-generator-plugin/src/coder/root/libs/modules/[module]/Coder.ts
@@ -223,13 +223,13 @@ export class Coder {
 
     if (!props.rightModelIsExternal) {
       await this.project.models[2].removeRelation();
+
       const rightModelContractsPath =
         this.project.models[2].project.model.project.contracts.project.extended
           .baseDirectory;
 
       const levelContractsPath = `${rightModelContractsPath}/src/lib/interfaces/sps-lite.ts`;
 
-      await this.project.models[2].createRelation();
       await this.project.relations[0].project.relation.project.contracts.project.root.detach(
         { levelContractsPath },
       );
@@ -237,6 +237,7 @@ export class Coder {
 
     if (!props.leftModelIsExternal) {
       await this.project.models[1].removeRelation();
+
       const leftModelContractsPath =
         this.project.models[1].project.model.project.contracts.project.extended
           .baseDirectory;
