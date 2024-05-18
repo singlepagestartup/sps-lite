1735916ce6a9bd8c0495c13fc51ef24930dd03bc
rogwild <rogwild.design@gmail.com>
2024-05-18 03:59:35 +0200

    create and remove relations with interfaces

--- libs/modules/sps-website-builder/models/slider/backend/schema/relations/root/src/lib/populate.ts
+++ libs/modules/sps-website-builder/models/slider/backend/schema/relations/root/src/lib/populate.ts
@@ -1,4 +1,7 @@
 import { populate as slidersToSlides } from "@sps/sps-website-builder-models-slider-backend-schema-relations-sliders-to-slides";
 import { populate as sliderBlocksToSliders } from "@sps/sps-website-builder-models-slider-backend-schema-relations-slider-blocks-to-sliders";
 
-export const populate = { ...slidersToSlides, ...sliderBlocksToSliders };
+export const populate = {
+  ...slidersToSlides,
+  ...sliderBlocksToSliders,
+};
