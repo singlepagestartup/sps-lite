ab0aa35e5bc99c78e6d6726c088828564a8b2d29
rogwild <flakedevcode@gmail.com>
2024-05-19 22:57:24 +0200

    dump

--- apps/frontend/src/db/dump.ts
+++ apps/frontend/src/db/dump.ts
@@ -1,15 +1,13 @@
 import { Dumper as SpsWebsiteBuilderDumper } from "@sps/sps-website-builder-backend-app";
+import { Dumper as StartupDumper } from "@sps/startup-backend-app";
 import { exit } from "process";
 
 (async () => {
   const spsWebsiteBuilderSeeder = new SpsWebsiteBuilderDumper();
   await spsWebsiteBuilderSeeder.dumpModels();
 
-  // const startupSeeder = new StartupSeeder({ seedResults });
-  // await startupSeeder.dumpModels();
-
-  // // await spsWebsiteBuilderSeeder.seedRelations();
-  // // await startupSeeder.seedRelations();
+  const startupSeeder = new StartupDumper();
+  await startupSeeder.dumpModels();
 })()
   .then(() => {
     exit(0);
