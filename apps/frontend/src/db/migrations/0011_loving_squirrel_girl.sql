d989615ed9291536dee35636008787896bb8b4de
rogwild <flakedevcode@gmail.com>
2024-05-24 00:52:06 +0200

    nullable title for slider

--- apps/frontend/src/db/migrations/0011_loving_squirrel_girl.sql
+++ apps/frontend/src/db/migrations/0011_loving_squirrel_girl.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "sps_w_b_slider" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "sps_w_b_slider" ADD COLUMN "title" text;--> statement-breakpoint
 ALTER TABLE "sps_w_b_slider" ADD COLUMN "class_name" text;
