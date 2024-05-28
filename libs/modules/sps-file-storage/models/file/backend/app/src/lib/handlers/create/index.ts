8bd19da2fd58fb45f2c4a005a974c28fc3cd848b
rogwild <flakedevcode@gmail.com>
2024-05-28 13:29:59 +0200

    file storage

--- libs/modules/sps-file-storage/models/file/backend/app/src/lib/handlers/create/index.ts
+++ libs/modules/sps-file-storage/models/file/backend/app/src/lib/handlers/create/index.ts
@@ -6,83 +6,82 @@ import path from "path";
 import fs from "fs/promises";
 import { MiddlewaresGeneric } from "@sps/shared-backend-api";
 
+// export const handler = async (
+//   c: Context<MiddlewaresGeneric, string, BlankInput>,
+//   next: Next,
+// ) => {
+//   const body = await c.req.parseBody();
+
+//   if (typeof body["data"] !== "string") {
+//     return next();
+//   }
+
+//   const data = JSON.parse(body["data"]);
+
+//   try {
+//     const entity = await model.services.create({ data });
+
+//     return c.json(
+//       {
+//         data: entity,
+//       },
+//       201,
+//     );
+//   } catch (error: any) {
+//     throw new HTTPException(400, {
+//       message: error.message,
+//     });
+//   }
+// };
+
 export const handler = async (
   c: Context<MiddlewaresGeneric, string, BlankInput>,
   next: Next,
 ) => {
   const body = await c.req.parseBody();
 
-  if (typeof body["data"] !== "string") {
-    return next();
-  }
+  const filesArray = [body["files"]];
 
-  const data = JSON.parse(body["data"]);
+  for (const file of filesArray) {
+    if (Array.isArray(file)) {
+      return;
+    }
 
-  try {
-    const entity = await model.services.create({ data });
+    if (typeof file === "string") {
+      return;
+    }
 
-    return c.json(
-      {
-        data: entity,
-      },
-      201,
-    );
-  } catch (error: any) {
-    throw new HTTPException(400, {
-      message: error.message,
-    });
+    const buffer = await (file as File).arrayBuffer();
+
+    const root = process.cwd();
+    const cuttedStoragePath = "sps-file-storage";
+    const storagePath = `public/${cuttedStoragePath}`;
+    const filePath = path.join(root, storagePath, file.name);
+
+    await fs.writeFile(filePath, Buffer.from(buffer));
+
+    const createdFileUrl = path.join("/", cuttedStoragePath, file.name);
+
+    if (body["data"] && typeof body["data"] !== "string") {
+      return next();
+    }
+
+    const data = body["data"] ? JSON.parse(body["data"]) : {};
+    data["url"] = createdFileUrl;
+
+    try {
+      const entity = await model.services.create({ data });
+
+      return c.json(
+        {
+          data: entity,
+        },
+        201,
+      );
+    } catch (error: any) {
+      throw new HTTPException(400, {
+        message: error.message,
+      });
+    }
   }
 };
-
-// export const handler = async (
-//   c: Context<Env, string, BlankInput>,
-//   next: Next,
-// ) => {
-//   const body = await c.req.parseBody();
-
-//   const filesArray = [body["files"]];
-
-//   for (const file of filesArray) {
-//     console.log(file);
-//     if (Array.isArray(file)) {
-//       return;
-//     }
-
-//     if (typeof file === "string") {
-//       return;
-//     }
-
-//     const buffer = await (file as File).arrayBuffer();
-
-//     // const root = path.join(process.cwd());
-//     const root = "";
-//     const storagePath = "public/sps-file-storage";
-//     const filePath = path.join(root, storagePath, file.name);
-
-//     await fs.writeFile(filePath, Buffer.from(buffer));
-
-//     const createdFileUrl = path.join("/", storagePath, file.name);
-
-//     if (typeof body["data"] !== "string") {
-//       return next();
-//     }
-
-//     const data = JSON.parse(body["data"]);
-//     data["url"] = createdFileUrl;
-
-//     try {
-//       const entity = await model.services.create({ data });
-
-//       return c.json(
-//         {
-//           data: entity,
-//         },
-//         201,
-//       );
-//     } catch (error: any) {
-//       throw new HTTPException(400, {
-//         message: error.message,
-//       });
-//     }
-//   }
-// };
