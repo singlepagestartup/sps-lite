a71b056f5b6653bbfea9cddfb4c64235dd4feaad
rogwild <flakedevcode@gmail.com>
2024-06-07 02:05:46 +0200

    fixed types

--- libs/modules/startup/frontend/root/src/lib/app/index.tsx
+++ libs/modules/startup/frontend/root/src/lib/app/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { IComponentProps } from "./interface";
 import { Component as Widget } from "@sps/startup-models-widget-frontend-component";
+import { cn } from "@sps/shared-frontend-utils-client";
 
 export function App(props: IComponentProps) {
   return (
-    <div data-module="startup" className={props.className || ""}>
+    <div data-module="startup" className={cn("w-full flex", props.className)}>
       <Widget
         isServer={props.isServer}
         variant="default"
