ab9e7df85e429f8dbe15fc6edc30b1d366f6254a
rogwild <flakedevcode@gmail.com>
2024-06-07 02:58:13 +0200

    IF NOT EXISTS

--- libs/modules/startup/backend/app/root/src/lib/db/migrations/0001_chief_ken_ellis.sql
+++ libs/modules/startup/backend/app/root/src/lib/db/migrations/0001_chief_ken_ellis.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "s_widget" ADD COLUMN "class_name" text;--> statement-breakpoint
-ALTER TABLE "s_widget" ADD COLUMN "title" text;
+ALTER TABLE "s_widget" ADD COLUMN IF NOT EXISTS "class_name" text;--> statement-breakpoint
+ALTER TABLE "s_widget" ADD COLUMN IF NOT EXISTS "title" text;
