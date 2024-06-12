853185d483a7dd1235d30698517bd9fa4cab2c1c
rogwild <flakedevcode@gmail.com>
2024-06-12 22:18:35 +0200

    fixed build

--- libs/shared/backend/api/src/lib/middlewares/interfaces.ts
+++ libs/shared/backend/api/src/lib/middlewares/interfaces.ts
@@ -1,10 +1,12 @@
 import type { MiddlewareGeneric as ParseQueryMiddleware } from "./parse-query";
 import type { MiddlewareGeneric as LoggerMiddleware } from "./logger";
 import type { MiddlewareGeneric as ParseBodyMiddleware } from "./parse-body";
+import { SessionMiddlewareGeneric } from "@sps/sps-rbac-backend-sdk";
 
 export type MiddlewaresGeneric = ParseQueryMiddleware &
   LoggerMiddleware &
-  ParseBodyMiddleware;
+  ParseBodyMiddleware &
+  SessionMiddlewareGeneric;
 
 export type ParseQueryMiddlewareGeneric = ParseQueryMiddleware;
 export type LoggerMiddlewareGeneric = LoggerMiddleware;
