579b7ce69723c2fabe3edbe2f8e647ada8b1c757
rogwild <flakedevcode@gmail.com>
2024-06-16 03:54:31 +0200

    bundle

--- apps/host/next.config.js
+++ apps/host/next.config.js
@@ -8,7 +8,7 @@ const NEXT_PUBLIC_HOST_URL =
   process.env["NEXT_PUBLIC_HOST_URL"] || "http://localhost:3000";
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled: process.env.BUNDLE_ANALYZER === "true",
 });
 
 function makeConfig() {
