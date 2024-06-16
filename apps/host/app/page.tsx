34015484fa269155758bf7b2f96527895cca5199
rogwild <flakedevcode@gmail.com>
2024-06-16 13:20:31 +0200

    main and url page

---
+++ apps/host/app/page.tsx
@@ -0,0 +1,34 @@
+import { api as spsWebsiteBuilderPageApi } from "@sps/sps-website-builder-models-page-frontend-api-server";
+import { Component as SpsWebsiteBuilderPage } from "@sps/sps-website-builder-models-page-frontend-component";
+// import QueryString from "qs";
+
+export const revalidate = 3600;
+
+export async function generateStaticParams() {
+  return spsWebsiteBuilderPageApi.fetch.urls();
+}
+
+export async function generateMetadata(props: any) {
+  return spsWebsiteBuilderPageApi.fetch.generateMetadata(props);
+}
+
+export default async function Page(props: {
+  params: { url?: string[] };
+  // searchParams?: { [key: string]: any };
+}) {
+  const url = props.params.url?.join("/") || "/";
+  const slashedUrl = url.startsWith("/") ? url : `/${url}`;
+  // const query = QueryString.stringify(props.searchParams);
+  // const hostUrl = query ? `${slashedUrl}?${query}` : slashedUrl;
+
+  return (
+    <SpsWebsiteBuilderPage
+      isServer={true}
+      variant="default"
+      hostUrl={slashedUrl}
+      data={{
+        url: slashedUrl,
+      }}
+    />
+  );
+}
