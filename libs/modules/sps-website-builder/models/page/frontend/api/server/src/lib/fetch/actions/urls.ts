e0b13e470be1f3a8d3888f7d380afa23533d02a5
rogwild <flakedevcode@gmail.com>
2024-06-16 02:53:52 +0200

    fixed build

--- libs/modules/sps-website-builder/models/page/frontend/api/server/src/lib/fetch/actions/urls.ts
+++ libs/modules/sps-website-builder/models/page/frontend/api/server/src/lib/fetch/actions/urls.ts
@@ -11,42 +11,47 @@ import {
 } from "@sps/shared-utils";
 
 export async function action() {
-  const options: NextRequestOptions = {
-    next: {
-      revalidate: 0,
-      tags: [route],
-    },
-  };
-
-  const res = await fetch(
-    `${BACKEND_URL}/api/sps-website-builder/pages/urls`,
-    options,
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const json = await res.json();
-
-  if (json.error) {
-    throw new Error(json.error.message || "Failed to fetch data");
-  }
-
-  const transformedData = transformResponseItem<IModel>(json);
-
-  const paths =
-    transformedData?.urls?.map(
-      (pageParams: { url: string; locale: string }) => {
-        return {
-          ...pageParams,
-          url:
-            pageParams.url === "/"
-              ? []
-              : pageParams.url.split("/").filter((p) => p !== ""),
-        };
+  try {
+    const options: NextRequestOptions = {
+      next: {
+        revalidate: 0,
+        tags: [route],
       },
-    ) || [];
+    };
 
-  return paths;
+    const res = await fetch(
+      `${BACKEND_URL}/api/sps-website-builder/pages/urls`,
+      options,
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const json = await res.json();
+
+    if (json.error) {
+      throw new Error(json.error.message || "Failed to fetch data");
+    }
+
+    const transformedData = transformResponseItem<IModel>(json);
+
+    const paths =
+      transformedData?.urls?.map(
+        (pageParams: { url: string; locale: string }) => {
+          return {
+            ...pageParams,
+            url:
+              pageParams.url === "/"
+                ? []
+                : pageParams.url.split("/").filter((p) => p !== ""),
+          };
+        },
+      ) || [];
+
+    return paths;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
