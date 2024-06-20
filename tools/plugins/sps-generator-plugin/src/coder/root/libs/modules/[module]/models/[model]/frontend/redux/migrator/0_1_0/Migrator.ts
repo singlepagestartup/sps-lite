c26cca98e372a5139cb2a53aeebab6d02b600910
rogwild <flakedevcode@gmail.com>
2024-06-20 15:15:42 +0200

    r

---
+++ tools/plugins/sps-generator-plugin/src/coder/root/libs/modules/[module]/models/[model]/frontend/redux/migrator/0_1_0/Migrator.ts
@@ -0,0 +1,76 @@
+import {
+  generateFiles,
+  offsetFromRoot,
+  updateJson,
+  updateProjectConfiguration,
+} from "@nx/devkit";
+import { Migrator as ParentMigrator } from "../Migrator";
+import path from "path";
+
+export class Migrator {
+  parent: ParentMigrator;
+
+  constructor({ parent }: { parent: ParentMigrator }) {
+    this.parent = parent;
+  }
+
+  async execute() {
+    const baseDirectory = this.parent.coder.baseDirectory;
+    const baseName = this.parent.coder.baseName;
+    const offsetFromRootProject = offsetFromRoot(baseDirectory);
+
+    const exists = this.parent.coder.tree.exists(baseDirectory);
+
+    if (!exists) {
+      throw new Error(`The directory ${baseDirectory} does not exist`);
+    }
+
+    const rootFiles = this.parent.coder.tree.children(baseDirectory);
+
+    for (const rootFile of rootFiles) {
+      const pathToFile = `${baseDirectory}/${rootFile}`;
+      const isFile = this.parent.coder.tree.isFile(pathToFile);
+
+      if (isFile) {
+        this.parent.coder.tree.delete(pathToFile);
+      }
+    }
+
+    const oldRtkApiImportPath = `${this.parent.coder.parent.project.api.project.client.baseName}`;
+    const newRtkApiImportPath = `${this.parent.coder.parent.project.api.project.client.absoluteName}`;
+
+    const indexFile = this.parent.coder.tree
+      .read(`${baseDirectory}/src/lib/index.tsx`)
+      .toString("utf8");
+
+    const newIndexFile = indexFile.replace(
+      new RegExp(oldRtkApiImportPath, "g"),
+      newRtkApiImportPath,
+    );
+
+    this.parent.coder.tree.write(
+      `${baseDirectory}/src/lib/index.ts`,
+      newIndexFile,
+    );
+
+    generateFiles(
+      this.parent.coder.tree,
+      path.join(__dirname, `files`),
+      baseDirectory,
+      {
+        template: "",
+        lib_name: baseName,
+        offset_from_root: offsetFromRootProject,
+      },
+    );
+
+    updateJson(this.parent.coder.tree, `tsconfig.base.json`, (json) => {
+      const updatedJson = { ...json };
+      const project = updatedJson.compilerOptions.paths[`${baseName}`];
+
+      delete updatedJson.compilerOptions.paths[`${baseName}`];
+
+      return updatedJson;
+    });
+  }
+}
