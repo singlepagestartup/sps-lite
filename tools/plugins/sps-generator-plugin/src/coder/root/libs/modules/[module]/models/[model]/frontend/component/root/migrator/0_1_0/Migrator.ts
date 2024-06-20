5abf116e35047562e6ecd0cded2463f0d62ae670
rogwild <flakedevcode@gmail.com>
2024-06-20 14:52:41 +0200

    root

--- tools/plugins/sps-generator-plugin/src/coder/root/libs/modules/[module]/models/[model]/frontend/component/root/migrator/0_1_0/Migrator.ts
+++ tools/plugins/sps-generator-plugin/src/coder/root/libs/modules/[module]/models/[model]/frontend/component/root/migrator/0_1_0/Migrator.ts
@@ -1,6 +1,7 @@
 import {
   generateFiles,
   offsetFromRoot,
+  updateJson,
   updateProjectConfiguration,
 } from "@nx/devkit";
 import { Migrator as ParentMigrator } from "../Migrator";
@@ -18,34 +19,41 @@ export class Migrator {
     const baseName = this.parent.coder.baseName;
     const offsetFromRootProject = offsetFromRoot(baseDirectory);
 
-    console.log(`🚀 ~ execute ~ baseDirectory:`, baseDirectory);
     const exists = this.parent.coder.tree.exists(baseDirectory);
 
     if (!exists) {
       throw new Error(`The directory ${baseDirectory} does not exist`);
     }
 
-    // generateFiles(
-    //   this.parent.coder.tree,
-    //   path.join(__dirname, `files`),
-    //   baseDirectory,
-    //   {
-    //     template: "",
-    //     lib_name: baseName,
-    //     offset_from_root: offsetFromRootProject,
-    //   },
-    // );
+    const rootFiles = this.parent.coder.tree.children(baseDirectory);
 
-    // this.parent.coder.tree.delete(`${baseDirectory}/tsconfig.spec.json`);
+    for (const rootFile of rootFiles) {
+      const pathToFile = `${baseDirectory}/${rootFile}`;
+      const isFile = this.parent.coder.tree.isFile(pathToFile);
 
-    // updateProjectConfiguration(this.parent.coder.tree, baseName, {
-    //   ...this.parent.coder.project,
-    //   targets: {
-    //     "tsc:build": {},
-    //   },
-    // });
+      if (isFile) {
+        this.parent.coder.tree.delete(pathToFile);
+      }
+    }
 
-    // this.parent.coder.tree.delete(`${baseDirectory}/.babelrc`);
-    // this.parent.coder.tree.delete(`${baseDirectory}/.eslintrc.json`);
+    generateFiles(
+      this.parent.coder.tree,
+      path.join(__dirname, `files`),
+      baseDirectory,
+      {
+        template: "",
+        lib_name: baseName,
+        offset_from_root: offsetFromRootProject,
+      },
+    );
+
+    updateJson(this.parent.coder.tree, `tsconfig.base.json`, (json) => {
+      const updatedJson = { ...json };
+      const project = updatedJson.compilerOptions.paths[`${baseName}`];
+
+      delete updatedJson.compilerOptions.paths[`${baseName}`];
+
+      return updatedJson;
+    });
   }
 }
