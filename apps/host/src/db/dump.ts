79da7707864570b7f4a78b4ca796a2f00474bce5
rogwild <flakedevcode@gmail.com>
2024-06-21 16:31:21 +0200

    new dump

--- apps/host/src/db/dump.ts
+++ apps/host/src/db/dump.ts
@@ -1,3 +1,4 @@
+import { Dumper as SpsHostDumper } from "@sps/sps-host/backend/app/root";
 import { Dumper as SpsWebsiteBuilderDumper } from "@sps/sps-website-builder/backend/app/root";
 import { Dumper as StartupDumper } from "@sps/startup/backend/app/root";
 import { Dumper as SpsFileStorageDumper } from "@sps/sps-file-storage/backend/app/root";
@@ -5,6 +6,8 @@ import { Dumper as SpsRbacDumper } from "@sps/sps-rbac/backend/app/root";
 import { exit } from "process";
 
 (async () => {
+  const spsHostSeeder = new SpsHostDumper();
+  await spsHostSeeder.dumpModels();
   const spsWebsiteBuilderSeeder = new SpsWebsiteBuilderDumper();
   await spsWebsiteBuilderSeeder.dumpModels();
   const spsFileStorageSeeder = new SpsFileStorageDumper();
