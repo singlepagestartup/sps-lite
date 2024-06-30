e91d046e16bdf9209671e6b59476b66b3f5c870c
rogwild <flakedevcode@gmail.com>
2024-06-30 02:32:14 +0200

    new dump

---
+++ libs/modules/sps-website-builder/backend/db/root/src/lib/migrations/0022_dusty_corsair.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "sps_w_b_layouts" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ls_to_fs" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ls_to_ns" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_metadata" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ma_to_ss_fe_se_me_fs_8k7" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_pages" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ps_to_ls" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ps_to_ma_k31" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ps_to_ws" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ws_to_ss_rc_me_ws_25m" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "sps_w_b_ws_to_sp_me_ws_xy7" CASCADE;
--- libs/providers/db/src/lib/drizzle/seed.ts
+++ libs/providers/db/src/lib/drizzle/seed.ts
@@ -4,7 +4,6 @@ import { db } from "./index";
 export const seed = async () => {
   try {
     console.log("Seeding database started...");
-    const pages = await db.query.SPSWBPage.findMany();
 
     console.log("Seeding database finished successfully");
     process.exit(0);
