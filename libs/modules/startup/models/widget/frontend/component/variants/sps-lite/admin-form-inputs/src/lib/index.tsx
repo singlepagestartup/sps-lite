76e16320a3eaf81136cbcd6ddb94f3b25bdda612
rogwild <flakedevcode@gmail.com>
2024-07-03 13:49:49 +0200

    startup

--- libs/modules/startup/models/widget/frontend/component/variants/sps-lite/admin-form-inputs/src/lib/index.tsx
+++ libs/modules/startup/models/widget/frontend/component/variants/sps-lite/admin-form-inputs/src/lib/index.tsx
@@ -1,14 +1,14 @@
 import { IComponentProps } from "./interface";
 import Client from "./client";
 import Server from "./server";
-import { ReduxProvider } from "@sps/startup/models/widget/frontend/redux";
+import { Provider as ApiProvider } from "@sps/startup/models/widget/frontend/api/client";
 
 export function Component(props: IComponentProps) {
   const Comp: any = props.isServer ? Server : Client;
 
   return (
-    <ReduxProvider>
+    <ApiProvider>
       <Comp {...props} />
-    </ReduxProvider>
+    </ApiProvider>
   );
 }
