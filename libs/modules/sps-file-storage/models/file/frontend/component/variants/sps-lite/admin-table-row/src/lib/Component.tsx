86d50382ee9b6ad6fd051e27086633d77b90c5c5
rogwild <flakedevcode@gmail.com>
2024-07-04 18:50:11 +0200

    admin table row

--- libs/modules/sps-file-storage/models/file/frontend/component/variants/sps-lite/admin-table-row/src/lib/Component.tsx
+++ libs/modules/sps-file-storage/models/file/frontend/component/variants/sps-lite/admin-table-row/src/lib/Component.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { ErrorBoundary, ModelEntityCard } from "@sps/ui-adapter";
+import { ErrorBoundary } from "@sps/ui-adapter";
 import { IComponentPropsExtended } from "./interface";
 import { api } from "@sps/sps-file-storage/models/file/frontend/api/client";
 import { Component as AdminForm } from "@sps/sps-file-storage/models/file/frontend/component/variants/sps-lite/admin-form";
 import { BACKEND_URL } from "@sps/shared-utils";
 import Image from "next/image";
+import { Component as ParentComponent } from "@sps/shared-frontend-components/sps-lite/admin/admin-table-row/Component";
 
 export function Component(props: IComponentPropsExtended) {
   const deleteEntity = api.delete();
@@ -18,28 +19,25 @@ export function Component(props: IComponentPropsExtended) {
   }, [deleteEntity]);
 
   return (
-    <div
-      data-module="sps-file-storage"
-      data-model="file"
-      data-id={props.data?.id || ""}
-      data-variant={props.variant}
-    >
-      <ModelEntityCard
-        onDeleteEntity={() => {
-          if (props.data?.id) {
-            deleteEntity.mutate({ id: props.data.id });
-          }
-        }}
-        data={props.data}
-        adminForm={
-          <AdminForm
-            isServer={false}
-            hostUrl={props.hostUrl}
-            variant="admin-form"
-            data={props.data}
-          />
+    <ParentComponent
+      id={props.data.id}
+      module="sps-file-storage"
+      name="file"
+      adminForm={
+        <AdminForm
+          isServer={false}
+          hostUrl={props.hostUrl}
+          variant="admin-form"
+          data={props.data}
+        />
+      }
+      onDelete={() => {
+        if (props.data?.id) {
+          deleteEntity.mutate({ id: props.data.id });
         }
-      >
+      }}
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 p-4 pt-6">
         <div className="flex flex-col gap-0.5 overflow-hidden">
           <p className="text-xs text-muted-foreground">Variant</p>
           <p className="truncate">{props.data.variant}</p>
@@ -60,7 +58,7 @@ export function Component(props: IComponentPropsExtended) {
             </ErrorBoundary>
           ) : null}
         </div>
-      </ModelEntityCard>
-    </div>
+      </div>
+    </ParentComponent>
   );
 }
