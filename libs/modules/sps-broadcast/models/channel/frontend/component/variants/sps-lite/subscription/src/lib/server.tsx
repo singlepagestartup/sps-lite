9b99151bb0a93dd6dd968952f854405c46586ab1
rogwild <flakedevcode@gmail.com>
2024-07-09 04:16:16 +0200

    wrapped error for subscription

--- libs/modules/sps-broadcast/models/channel/frontend/component/variants/sps-lite/subscription/src/lib/server.tsx
+++ libs/modules/sps-broadcast/models/channel/frontend/component/variants/sps-lite/subscription/src/lib/server.tsx
@@ -9,22 +9,29 @@ import { Component } from "./Component";
 
 // default is required for dynamic import
 export default async function Server(props: IComponentProps) {
-  if (!props.data.id) {
-    return <></>;
+  try {
+    if (!props.data.id) {
+      return <></>;
+    }
+
+    const data = await api.findById({
+      id: props.data.id,
+      ...props.apiProps,
+    });
+
+    if (!data) {
+      return <></>;
+    }
+
+    return (
+      <ErrorBoundary fallback={Error}>
+        <Component {...props} data={data} />
+      </ErrorBoundary>
+    );
+  } catch (error) {
+    console.log(
+      `sps-boradcast ~ channel ~ subscription ~ Server ~ error:`,
+      error,
+    );
   }
-
-  const data = await api.findById({
-    id: props.data.id,
-    ...props.apiProps,
-  });
-
-  if (!data) {
-    return <></>;
-  }
-
-  return (
-    <ErrorBoundary fallback={Error}>
-      <Component {...props} data={data} />
-    </ErrorBoundary>
-  );
 }
