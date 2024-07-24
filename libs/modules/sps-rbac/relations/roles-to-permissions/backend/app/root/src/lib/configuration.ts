760ce478b3b1308640c4349fe3ccd7d0c2fe619b
rogwild <flakedevcode@gmail.com>
2024-07-24 11:28:09 +0200

    seed config

--- libs/modules/sps-rbac/relations/roles-to-permissions/backend/app/root/src/lib/configuration.ts
+++ libs/modules/sps-rbac/relations/roles-to-permissions/backend/app/root/src/lib/configuration.ts
@@ -28,6 +28,42 @@ export class Configuration extends ParentConfiguration {
           module: "sps-rbac",
           name: "roles-to-permissions",
           type: "relation",
+          transformers: [
+            {
+              field: "roleId",
+              transform: (data) => {
+                const relationEntites = data.seeds
+                  .find(
+                    (seed) =>
+                      seed.name === "role" &&
+                      seed.type === "model" &&
+                      seed.module === "sps-rbac",
+                  )
+                  ?.seeds?.filter(
+                    (seed) => seed.dump.id === data.entity.dump.roleId,
+                  );
+
+                return relationEntites?.[0].new.id;
+              },
+            },
+            {
+              field: "permissionId",
+              transform: (data) => {
+                const relationEntites = data.seeds
+                  .find(
+                    (seed) =>
+                      seed.name === "permission" &&
+                      seed.type === "model" &&
+                      seed.module === "sps-rbac",
+                  )
+                  ?.seeds?.filter(
+                    (seed) => seed.dump.id === data.entity.dump.permissionId,
+                  );
+
+                return relationEntites?.[0].new.id;
+              },
+            },
+          ],
         },
       },
     });
