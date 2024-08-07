e351acef62ab5efe6277d559a7d24fd36a734915
rogwild <flakedevcode@gmail.com>
2024-08-07 05:39:04 +0200

    file upload

--- libs/modules/sps-file-storage/models/file/backend/app/api/src/lib/controller.ts
+++ libs/modules/sps-file-storage/models/file/backend/app/api/src/lib/controller.ts
@@ -45,15 +45,41 @@ export class Controller extends RESTController<(typeof Table)["$inferSelect"]> {
   }
 
   async create(c: Context, next: any): Promise<Response> {
-    if (!c.var.parsedBody) {
+    const body = await c.req.parseBody();
+
+    if (!body) {
       throw new HTTPException(400, {
         message: "Invalid body",
       });
     }
 
-    if (!c.var.parsedBody.files) {
+    const parsedBody: {
+      data?: {
+        [key: string]: any;
+      };
+      files?: {
+        [key: string]: File;
+      };
+    } = {};
+
+    Object.keys(body).forEach((key) => {
+      if (body[key] instanceof File) {
+        const file = body[key] as File;
+
+        if (!parsedBody.files) {
+          parsedBody.files = {};
+        }
+
+        parsedBody.files = {
+          ...parsedBody.files,
+          [key]: file,
+        };
+      }
+    });
+
+    if (!parsedBody.files) {
       const entity = await this.service.create({
-        data: c.var.parsedBody.data,
+        data: parsedBody.data,
       });
 
       return c.json(
@@ -64,7 +90,13 @@ export class Controller extends RESTController<(typeof Table)["$inferSelect"]> {
       );
     }
 
-    for (const [name, file] of Object.entries(c.var.parsedBody.files)) {
+    if (body?.["data"]) {
+      if (typeof body["data"] === "string") {
+        parsedBody.data = JSON.parse(body["data"]);
+      }
+    }
+
+    for (const [name, file] of Object.entries(parsedBody.files)) {
       if (Array.isArray(file)) {
         throw new HTTPException(400, {
           message: "Multiple files are not allowed",
@@ -83,7 +115,7 @@ export class Controller extends RESTController<(typeof Table)["$inferSelect"]> {
       const cuttedStoragePath = "images";
       const storagePath = `public/${cuttedStoragePath}`;
 
-      const data = c.var.parsedBody.data ?? {};
+      const data: any = parsedBody.data ?? {};
       data[name] = "";
       const createdEntity = await this.service.create({ data });
 
@@ -105,6 +137,7 @@ export class Controller extends RESTController<(typeof Table)["$inferSelect"]> {
       );
 
       data[name] = createdFileUrl;
+
       const entity = await this.service.update({
         id: createdEntity.id,
         data,
@@ -122,6 +155,139 @@ export class Controller extends RESTController<(typeof Table)["$inferSelect"]> {
       message: "Invalid file",
     });
   }
+
+  async update(c: Context, next: any): Promise<Response> {
+    const uuid = c.req.param("uuid");
+
+    if (!uuid) {
+      return c.json(
+        {
+          message: "Invalid id",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const body = await c.req.parseBody();
+
+    if (!body) {
+      throw new HTTPException(400, {
+        message: "Invalid body",
+      });
+    }
+
+    const parsedBody: {
+      data?: {
+        [key: string]: any;
+      };
+      files?: {
+        [key: string]: File;
+      };
+    } = {};
+
+    Object.keys(body).forEach((key) => {
+      if (body[key] instanceof File) {
+        const file = body[key] as File;
+
+        if (!parsedBody.files) {
+          parsedBody.files = {};
+        }
+
+        parsedBody.files = {
+          ...parsedBody.files,
+          [key]: file,
+        };
+      }
+    });
+
+    if (body?.["data"]) {
+      if (typeof body["data"] === "string") {
+        parsedBody.data = JSON.parse(body["data"]);
+      }
+    }
+
+    if (!parsedBody.files) {
+      const entity = await this.service.create({
+        data: parsedBody.data,
+      });
+
+      return c.json(
+        {
+          data: entity,
+        },
+        201,
+      );
+    }
+
+    for (const [name, file] of Object.entries(parsedBody.files)) {
+      if (Array.isArray(file)) {
+        throw new HTTPException(400, {
+          message: "Multiple files are not allowed",
+        });
+      }
+
+      if (typeof file === "string") {
+        throw new HTTPException(400, {
+          message: "Invalid file type",
+        });
+      }
+
+      const buffer = await (file as File).arrayBuffer();
+
+      const extension = file.name.split(".").pop();
+
+      if (!extension) {
+        throw new Error("Invalid file extension");
+      }
+
+      const root = process.cwd();
+      const cuttedStoragePath = "images";
+      const storagePath = `public/${cuttedStoragePath}`;
+
+      const fileName = await this.service.getUniqueFileName({ extension });
+      const filePath = path.join(root, storagePath, fileName + "." + extension);
+
+      await fs.writeFile(filePath, Buffer.from(buffer));
+
+      const createdFileUrl = path.join(
+        "/",
+        cuttedStoragePath,
+        fileName + "." + extension,
+      );
+
+      const data: any = parsedBody.data ?? {};
+      data[name] = createdFileUrl;
+
+      const previous = await this.service.findById({ id: uuid });
+
+      const entity = await this.service.update({ id: uuid, data });
+
+      if (previous?.file) {
+        const root = process.cwd();
+        const filePath = path.join(root, "public", previous.file);
+
+        try {
+          await fs.unlink(filePath);
+        } catch (error: any) {
+          console.error("error", error.toString());
+        }
+      }
+
+      return c.json(
+        {
+          data: entity,
+        },
+        201,
+      );
+    }
+
+    throw new HTTPException(400, {
+      message: "Invalid file",
+    });
+  }
+
   catch(error: any) {
     throw new HTTPException(400, {
       message: error.message,
