6d6066e09c9dbdaf1817a0478254157cbf2b2984
rogwild <flakedevcode@gmail.com>
2024-08-25 17:06:06 +0200

    defined

--- libs/shared/utils/src/lib/envs.ts
+++ libs/shared/utils/src/lib/envs.ts
@@ -108,4 +108,4 @@ export const O_X_PROCESSING_WEBHOOK_PASSWORD =
 
 export const AWS_ACCESS_KEY_ID = process.env["AWS_ACCESS_KEY_ID"];
 export const AWS_SECRET_ACCESS_KEY = process.env["AWS_SECRET_ACCESS_KEY"];
-export const AWS_REGION = process.env["AWS_REGION"] || "eu-central-1"; // Frankfurt
+export const AWS_REGION = process.env["AWS_REGION"];
