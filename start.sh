d19d5fd0d3b36c92b3184e6e44bd7632a9146974
rogwild <flakedevcode@gmail.com>
2024-08-28 03:37:24 +0200

    start command

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Run host app
 # if first passed argument is "host" run npm run host:start
 if [ "$1" = "host" ]; then
+  ./create_env.sh deployment
   npx nx run host:db:seed &
   npm run host:start
   exit 0
